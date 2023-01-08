87b5305543cfeef1b36e07e28bbd33cb720fac8f
alan <alan@compendiumdev.co.uk>
2023-01-08 14:44:40 +0000

    (poc) generate animation in html

--- src/graphvizStepParser.test.ts
+++ src/graphvizStepParser.test.ts
@@ -191,7 +191,7 @@ digraph G {
 
 });
 
-describe.skip("prototype file reading and writing", () => {
+describe("prototype file reading and writing", () => {
 
     function readAndParseFile(filePath:string): GraphvizStepParser{
 
@@ -241,6 +241,41 @@ describe.skip("prototype file reading and writing", () => {
               }
             fileCount++;
         }
+    });
 
+    it("can create an animation html from a template", ()=>{
+
+        const path = require('path');
+
+        const inputFolder = path.resolve( __dirname,  `../examples/`);
+        const inputFileName = `001-meta-model-of-software-testing.test.dot`;
+        const templateName =`demo-template.html`;
+        const outputFolder = path.resolve( __dirname,  `../examples/output/`);
+
+        const parsed=readAndParseFile(path.resolve(inputFolder,inputFileName));
+
+        const fs = require('fs');
+        const outputTemplate = fs.readFileSync(path.resolve(inputFolder,templateName), 'utf8');
+
+
+        if (!fs.existsSync(outputFolder)){
+            fs.mkdirSync(outputFolder, { recursive: true });
+        }
+
+        let dotForTemplate = "";
+
+        for(const dotFileContent of parsed.dotVersions){
+            let writeDotFileContent = dotFileContent.replace(/\l/gm, "\\l");
+            dotForTemplate = dotForTemplate + "`\n" + writeDotFileContent + "`,\n";
+        }
+
+        const demoOutput = outputTemplate.replace("// INSERT DOT DATA HERE",dotForTemplate);
+        try {
+            fs.writeFileSync(path.resolve(outputFolder, inputFileName + ".demo.html"), demoOutput);
+            // file written successfully
+          } catch (err) {
+            console.error("ERROR Writing Demo HTML");
+            console.error(err);
+          }
     });
 });
